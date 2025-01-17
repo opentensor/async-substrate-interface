8fd6324c1aac701c5520d34e2daf4f5a35ed7cb8
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-01-16 19:52:20 -0800

    Bumps version and changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 1.0.0rc3 /2025-01-17
+
+## What's Changed
+* Adds nonce implementation @ibraheem-opentensor in https://github.com/opentensor/async-substrate-interface/pull/8
+
 ## 1.0.0rc2 /2025-01-15
 
 ## What's Changed
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="async-substrate-interface",
-    version="1.0.0rc2",
+    version="1.0.0rc3",
     description="Asyncio library for interacting with substrate.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
