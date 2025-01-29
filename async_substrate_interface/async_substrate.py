2f6774629128e57b74a3378721f230e5816f010e
Benjamin Himes <benjamindhimes@gmail.com>
2025-01-30 00:25:42 +0200

    Fixes decode-scale by setting the object to None correctly.

--- async_substrate_interface/async_substrate.py
+++ async_substrate_interface/async_substrate.py
@@ -869,12 +869,13 @@ class AsyncSubstrateInterface(SubstrateMixin):
         """
         if scale_bytes == b"\x00":
             obj = None
-        if type_string == "scale_info::0":  # Is an AccountId
-            # Decode AccountId bytes to SS58 address
-            return bytes.fromhex(ss58_decode(scale_bytes, SS58_FORMAT))
         else:
-            await self._wait_for_registry(_attempt, _retries)
-            obj = decode_by_type_string(type_string, self.registry, scale_bytes)
+            if type_string == "scale_info::0":  # Is an AccountId
+                # Decode AccountId bytes to SS58 address
+                return bytes.fromhex(ss58_decode(scale_bytes, SS58_FORMAT))
+            else:
+                await self._wait_for_registry(_attempt, _retries)
+                obj = decode_by_type_string(type_string, self.registry, scale_bytes)
         if return_scale_obj:
             return ScaleObj(obj)
         else:
