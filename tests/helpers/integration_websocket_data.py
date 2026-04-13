b5f1f4f39f293e3c7c0039c5318a74a683e5fe99
BD Himes <b@latent.to>
2026-04-13 13:27:21 +0200

    Init bringover from bittensor of script

---
+++ tests/helpers/integration_websocket_data.py
@@ -0,0 +1,9 @@
+# This is dictionary of raw websocket send vs recv data for given methods used for integration testing, essentially
+# stubbing the network I/O only
+# This is basically a JSON file, but is not a JSON file because of the discrepancies between JSON and Python types
+
+from itertools import cycle
+
+
+WEBSOCKET_RESPONSES = {
+}
